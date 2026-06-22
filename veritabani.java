4664327deb221fef2368f5de40506a9c97edd17a
ferhat yılmaz <ferhatylmz1159@gmail.com>
2026-06-22 22:55:27 +0300

    Create veritabani.java

---
+++ veritabani.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Veritabani {
+
+    public static String aktifKullanici = "";
+
+    public static int sonTutar = 0;
+
+    public static ArrayList<String> kiralamaGecmisi = new ArrayList<>();
+
+}
